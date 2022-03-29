777ca3392da05e8ca1076fe521e97bc287958398
Devid Menasci <97224404+DevidMen@users.noreply.github.com>
2022-03-29 22:54:53 +0300

    Milestone6

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,17 +9,94 @@ function Home(props) {
 
     const {currentUser,tweetList} = props
 
-    return (
+const allData = [...tweetList]
+const perPage = 10;
+const types = {
+  start: "START",
+  loaded: "LOADED"
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case types.start:
+      return { ...state, loading: true };
+    case types.loaded:
+      return {
+        ...state,
+        loading: false,
+        data: [...state.data, ...action.newData],
+        more: action.newData.length === perPage,
+        after: state.after + action.newData.length
+      };
+    default:
+      throw new Error("Don't understand action");
+  }
+};
+const [state, dispatch] = useReducer(reducer, {
+  loading: false,
+  more: true,
+  data: [],
+  after: 0
+});
+const { loading, data, after, more } = state;
+
+const load = () => {
+  dispatch({ type: types.start });
+
+  setTimeout(() => {
+    const newData = allData.slice(after, after + perPage);
+    dispatch({ type: types.loaded, newData });
+  }, 300);}
+
+
+  const loader = useRef(load);
+  const observer = useRef(
+    new IntersectionObserver(
+      entries => {
+        const first = entries[0];
+        if (first.isIntersecting) {
+          loader.current();
+        }
+      },
+      { threshold: 0 }
+    )
+  );
+  const [element, setElement] = useState(null);
+
+  useEffect(() => {
+    loader.current = load;
+  }, [load]);
+
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
+  return (
                     <div className='tweetList'>
                     <CreateTweet/>
-                        {tweetList.map((element => <TweetList
+                        {data.map((element => <TweetList
                         
                             element={element}
                             key={element.id}
                            
                           currentUser = {currentUser.find((el) => { return el.key === element.key})}
                         />))}
-                    
+                    {loading && <li>Loading...</li>}
+
+{!loading && more && (
+  <li ref={setElement} style={{ background: "transparent" }}></li>
+)}
                 </div>
 
     )
